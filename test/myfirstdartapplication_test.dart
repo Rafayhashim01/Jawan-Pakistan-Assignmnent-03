dbd7da6233137a47bd71e24d199d96de03aa0111
Abdul Rafay Hashim <rafayhashim01@gmail.com>
2023-06-24 16:28:30 +0500

    1st Question

---
+++ test/myfirstdartapplication_test.dart
@@ -0,0 +1,8 @@
+import 'package:myfirstdartapplication/myfirstdartapplication.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
